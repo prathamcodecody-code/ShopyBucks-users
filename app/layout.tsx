ac091c6120ad293d68d689038136dfab3b276a98
prathamcodecody-code <pratham.codecody@gmail.com>
2026-01-19 16:01:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import Footer from "@/components/Footer";
 import { AuthProvider } from "@/app/context/AuthContext";
 import { WishlistProvider } from "@/app/context/WishlistContext";
 import { CheckoutProvider } from "@/app/context/CheckoutContext";
+import { AuthModalProvider } from "@/app/auth/AuthModalContext";
 import { Toaster } from "react-hot-toast";
+import AuthModalWrapper from "@/components/Home/AuthModalWrapper";
 
 export default function RootLayout({
   children,
@@ -17,17 +19,21 @@ export default function RootLayout({
         <AuthProvider>
           <WishlistProvider>
             <CheckoutProvider>
+              <AuthModalProvider>
 
-              <Toaster position="top-right" />
+                <Toaster position="top-right" />
 
-              <Navbar />
+                <Navbar />
 
-              <main className="min-h-[calc(100vh-160px)]">
-                {children}
-              </main>
+                <main className="min-h-[calc(100vh-160px)]">
+                  {children}
+                </main>
 
-              <Footer />
+                <Footer />
 
+                <AuthModalWrapper />
+
+              </AuthModalProvider>
             </CheckoutProvider>
           </WishlistProvider>
         </AuthProvider>
