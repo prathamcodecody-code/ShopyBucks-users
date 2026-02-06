8cebb3ae780c837063486d68994317c7d04bc7aa
prathamcodecody-code <pratham.codecody@gmail.com>
2026-02-06 12:38:20 +0530

    Update AuthModalWrapper.tsx

--- components/Home/AuthModalWrapper.tsx
+++ components/Home/AuthModalWrapper.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import AuthModal from "@/app/auth/AuthModal";
 import { useAuthModal } from "@/app/auth/AuthModalContext";
+import AuthModal from "@/app/auth/AuthModal";
+import RegisterModal from "@/app/auth/RegisterModal";
 
 export default function AuthModalWrapper() {
-  const { isOpen, closeAuthModal } = useAuthModal();
-  
-  return <AuthModal show={isOpen} onClose={closeAuthModal} />;
+  const { mode, close } = useAuthModal();
+
+  if (!mode) return null;
+
+  if (mode === "login") {
+    return <AuthModal show onClose={close} />;
+  }
+
+  if (mode === "register") {
+    return <RegisterModal show onClose={close} />;
+  }
+
+  return null;
 }
